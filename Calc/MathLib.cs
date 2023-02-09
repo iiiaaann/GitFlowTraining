4ad103006d68f0a2dd00f2c8e52d0db87227269d
Nicolas Glassey <nicolas.glassey@cpnv.ch>
2023-02-09 14:24:17 +0100

    Add project files.

---
+++ Calc/MathLib.cs
@@ -0,0 +1,7 @@
+﻿namespace Calc
+{
+    public class MathLib
+    {
+
+    }
+}
---
+++ TestCalc/TestMatLib.cs
@@ -0,0 +1,16 @@
+namespace TestCalc
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.False(true);
+        }
+    }
+}
